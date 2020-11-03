7e022027bd9ed8424f1ccd02d1d6adeedfd6ad05
Jim Raden <james.raden@gmail.com>
2020-11-03 13:48:42 -0500

    Add a child iframe; output the sessionID

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.dir(req.cookies);
-  res.render("index", { title: "Express" });
+  res.render("index", {
+    title: "Express",
+    sessionID: req.sessionID,
+  });
+});
+
+router.get("/child", function (req, res, next) {
+  console.dir("Child cookies", req.cookies);
+  res.render("child", { sessionID: req.sessionID });
 });
 
 module.exports = router;
